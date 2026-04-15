1676b3d8878097edbe12df0f19768834d7f5e913
Ruben Oostinga <roostinga@xebia.com>
2026-04-15 15:36:47 +0200

    Remove solution

--- src/main/java/PascalsTriangle.java
+++ src/main/java/PascalsTriangle.java
@@ -6,19 +6,6 @@ import static java.util.Arrays.asList;
 class PascalsTriangle {
 
     List<Integer> triangleRow(int n) {
-        if (n == 0) {
-            return asList(1);
-        }
-
-        var previousRow = triangleRow(n - 1);
-
-        var row = new ArrayList<Integer>();
-        row.add(1);
-        for (int col = 1; col < n; col++) {
-            row.add(previousRow.get(col - 1) + previousRow.get(col) + 1);
-        }
-        row.add(1);
-
-        return row;
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation to pass the tests");
     }
 }
